436f1e6d83150afcc37f077b5e153131b678c6fd
Rawand <rawandhawiz@gmail.com>
2016-03-09 17:40:06 +0000

    -added extra command line parameters

---
+++ breadcrumb_web_crawler/run.bat
@@ -0,0 +1,6 @@
+C:\Python27\ArcGIS10.3\python.exe get-pip.py
+C:\Python27\ArcGIS10.3\Scripts\pip.exe install beautifulsoup4
+C:\Python27\ArcGIS10.3\Scripts\pip.exe install requests
+C:\Python27\ArcGIS10.3\Scripts\pip.exe install click
+C:\Python27\ArcGIS10.3\Scripts\pip.exe install requests
+C:\Python27\ArcGIS10.3\python.exe web_crawl.py -s sites.csv -o new_sites.csv -sd -l 100
